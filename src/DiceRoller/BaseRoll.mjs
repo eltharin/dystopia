00e03169db87be88d999af06011ac8241de1e97c
eltharin <eltharin18@hotmail.fr>
2026-05-31 14:26:40 +0200

    change localizeSystem for argument and rolldice

--- src/DiceRoller/BaseRoll.mjs
+++ src/DiceRoller/BaseRoll.mjs
@@ -5,41 +5,18 @@ export class BaseRoll extends Roll {
     constructor(formula="", data={}, options={}) {
         super(formula, data, options);
         
-        if(this.options.coutFletrine == undefined) {
-            this.options.coutFletrine = 0;            
-        }
-        
-        if(this.options.coutFletrineOk == undefined) {
-            this.options.coutFletrineOk = false;            
-        }
     }
 
     async _prepareChatRenderContext({flavor, isPrivate=false, ...options}={}) {
         let ret = await super._prepareChatRenderContext({flavor, isPrivate, ...options});
-        ret.result = game.i18n.format(this.getResultText());
-        ret.totalText = this.getTotalText();
-        ret.totalValue = this.getTotalValue();
-        ret.seuil = this.getSeuil();
-
-        ret.fletrineTooltip = await this.getFletrineTooltip();
+       // ret.result = game.i18n.format(this.getResultText());
+        //ret.totalText = this.getTotalText();
+        //ret.totalValue = this.getTotalValue();
+        //ret.seuil = this.getSeuil();
 
         return ret;
     }
     
-    async getFletrineTooltip() {
-        
-        return foundry.applications.handlebars.renderTemplate(system.Consts.TEMPLATES_PATH + "/dice/affect_fletrine.hbs", {
-            isMagie: this.isCompetenceMagie(),
-            nb: this.options.coutFletrine, 
-            ok: this.options.coutFletrineOk
-        });
-    }
-
-    isCompetenceMagie()
-    {
-        console.error("can't have resolve isCompetenceMAgie on " + this.constructor.name);
-    }
-
     getActor()
     {
         console.error("can't have resolve getToken on " + this.constructor.name);
@@ -54,21 +31,8 @@ export class BaseRoll extends Roll {
         return super.fromData(data);
     }
 
-    async toMessage(messageData = {}, options = {}) {
+   /* async toMessage(messageData = {}, options = {}) {
         const msg = await super.toMessage(messageData, options);
-
-        if(this instanceof BaseRoll && this.isCompetenceMagie()) {
-            if(this.getActor().isSurchauffe) {
-                const myRoll = new system.DiceRoller.SurchauffeRoll("1d12",{}, {});
-                myRoll.toMessage({
-                    async: true, 
-                    flavor: "Surchauffe",
-                    speaker: ChatMessage.getSpeaker({ alias: this.getActor().actor.name + " ( " + game.user.name + " )"}),
-                });
-            } else if (this.getActor().isDissonnance) {
-                system.Actor.fct.enterSurchauffe(this.getActor().actor);
-            }
-        }
 
         return msg;
     }
@@ -80,17 +44,7 @@ export class BaseRoll extends Roll {
         ]
     }
 
-    convertTotal(tableau)
-    {
-        return tableau.map(e => {
-            if(Array.isArray(e)) {
-                return [e[0] || 0, e[1] || "+"];
-            }
-            else {
-                return [e || 0, "+"];
-            }
-        })
-    }
+
 
     getTotalText()
     {
@@ -122,5 +76,5 @@ export class BaseRoll extends Roll {
     getSeuil()
     {
         return 0;
-    }
+    }*/
 }
